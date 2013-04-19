50a75143dc9d3eccacf5f1fe8cead5fbe8261ccb
ferhat elmas <elmas.ferhat@gmail.com>
2013-04-19 19:47:30 +0200

    silly me, forgot data file

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from distutils.core import setup
 
 setup(
     name='SexMachine',
-    version='0.1.0',
+    version='0.1.1',
     author='Ferhat Elmas',
     author_email='elmas.ferhat@gmail.com',
     packages=['sexmachine', 'sexmachine.test'],
+    package_dir={'sexmachine': 'sexmachine'},
+    package_data={'sexmachine': ['data/*.txt']},
     url='https://github.com/ferhatelmas/sexmachine/',
     license='GPLv3',
     description='Get the gender from first name.',
